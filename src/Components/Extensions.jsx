a03913502a20d529928bf70cf6103d8b08a738ac
JudithJude369 <ifunanyajudith09@gmail.com>
2025-08-19 20:39:10 +0100

    updating all on page load

--- src/Components/Extensions.jsx
+++ src/Components/Extensions.jsx
@@ -2,26 +2,35 @@ import { useGlobalContext } from "@/Hooks/context";
 import Button from "./Button";
 
 const Extensions = () => {
-  const { theme, setFilter } = useGlobalContext();
+  const { theme, setFilter, filter } = useGlobalContext();
   return (
     <div className="extensions-container">
       <h1 className={theme ? "text-dark" : "text-light"}>Extensions List </h1>
       <div className="btn-container">
         <Button
           onClick={() => setFilter("all")}
-          className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
+          className={`${theme ? "extensionsBtn-dark" : "extensionsBtn-light"} ${
+            filter === "all" ? "activeBtn" : ""
+          }`}
+          // className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
         >
           All
         </Button>
         <Button
           onClick={() => setFilter("active")}
-          className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
+          className={`${theme ? "extensionsBtn-dark" : "extensionsBtn-light"} ${
+            filter === "active" ? "activeBtn" : ""
+          }`}
+          // className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
         >
           Active
         </Button>
         <Button
           onClick={() => setFilter("inactive")}
-          className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
+          className={`${theme ? "extensionsBtn-dark" : "extensionsBtn-light"} ${
+            filter === "inactive" ? "activeBtn" : ""
+          }`}
+          // className={theme ? "extensionsBtn-dark" : "extensionsBtn-light"}
         >
           Inactive
         </Button>
